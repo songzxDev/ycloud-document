35265223dec253b69557f4ede353eaeb8da5d530
liuxu7 <liuxu7@yonyou.com>
2017-12-19 17:09:18 +0800

    暂存

--- src/views/button/index.js
+++ src/views/button/index.js
@@ -16,8 +16,29 @@ function init () {
         <y-button params="type: 'warning'">Warning</y-button>
         <y-button params="type: 'error'">Error</y-button>
       </template>
-      `
-    }
+      `,
+      size: `
+      <template>
+        <y-button params="type: 'primary', size: 'large'">Large</y-button>
+        <y-button params="type: 'primary'">Default</y-button>
+        <y-button params="type: 'primary', size: 'small'">Small</y-button>
+        <y-button params="type: 'primary', shape: 'circle', size: 'large'">Large</y-button>
+        <y-button params="type: 'primary', shape: 'circle'">Default</y-button>
+        <y-button params="type: 'primary', shape: 'circle', size: 'small'">Small</y-button>
+      </template>
+      `,
+      disabled: `
+      <template>
+        <y-button params="type: 'primary'">Default</y-button>
+        <y-button params="type: 'primary', disabled: true">Default(Disabled)</y-button>
+      </template>
+      `,
+      loading: ``
+    },
+    load: function () {
+      console.log(this)
+    }.apply(viewmodel),
+    loading: ko.observable(false)
   }
 
   ko.applyBindings(viewmodel, document.getElementById('app'))
