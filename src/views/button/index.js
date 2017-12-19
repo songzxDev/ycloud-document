c979e12fee86725183388ed4625472aab69e8943
liuxu7 <liuxu7@yonyou.com>
2017-12-19 14:48:58 +0800

    .

--- src/views/button/index.js
+++ src/views/button/index.js
@@ -3,9 +3,21 @@ import ko from 'knockout'
 import '@/components'
 function init () {
   var viewmodel = {
-    code: `
-      <y-button type="primary">按钮</y-button>
-    `
+    code: {
+      type: `
+      <template>
+        <y-button>Default</y-button>
+        <y-button params="type: 'primary', size: 'large'">Primary</y-button>
+        <y-button params="type: 'ghost', size: 'large'">Ghost</y-button>
+        <y-button params="type: 'dashed'">Dashed</y-button>
+        <y-button params="type: 'text'">Text</y-button>
+        <y-button params="type: 'info'">Info</y-button>
+        <y-button params="type: 'success'">Success</y-button>
+        <y-button params="type: 'warning'">Warning</y-button>
+        <y-button params="type: 'error'">Error</y-button>
+      </template>
+      `
+    }
   }
 
   ko.applyBindings(viewmodel, document.getElementById('app'))
