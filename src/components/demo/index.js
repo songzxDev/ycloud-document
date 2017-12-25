78e9effa5719a581db09c7d592f27080dee47a8a
windknow <songhlc@yonyou.com>
2017-12-25 20:01:19 +0800

    添加grid组件demo

--- src/components/demo/index.js
+++ src/components/demo/index.js
@@ -4,6 +4,10 @@ import '../code'
 import ko from 'knockout'
 function init (params) {
   this.code = params.code
+  this.isShowAll = ko.observable(false)
+  this.toggle = function () {
+    this.isShowAll(!this.isShowAll())
+  }.bind(this)
 }
 ko.components.register('demo', {
   viewModel: init,
--- src/views/grid/index.js
+++ src/views/grid/index.js
@@ -19,6 +19,46 @@ function init () {
   })
   var viewmodel = {
     rows: table.rows,
+    columnsRender: [
+      {
+        title: 'id',
+        field: 'id'
+      }, {
+        title: 'name',
+        field: 'name',
+        type: 'render',
+        renderFn (row) {
+          return `姓名：<span>${row.getValue('name')}</span>, 单价：<span>${row.getValue('price')}</span>`
+        }
+      }
+    ],
+    columnsBase: [
+      {
+        title: 'id',
+        field: 'id'
+      }, {
+        title: 'name',
+        field: 'name'
+      }
+    ],
+    columnsIndex: [
+      {
+        title: 'id',
+        field: 'id',
+        type: 'checkbox'
+      }, {
+        title: '序号',
+        field: 'id',
+        type: 'index',
+        width: '80'
+      }, {
+        title: 'id',
+        field: 'id',
+      }, {
+        title: 'name',
+        field: 'name'
+      }
+    ],
     columns: [{
       title: 'id',
       field: 'id'
@@ -45,6 +85,35 @@ function init () {
       title: '总价',
       field: 'total'
     }],
+    code2: `
+      <template>
+        <y-grid params="isDataTable:true,rows:$parent.rows, columns: $parent.columnsRender"></y-grid>
+      </template>  
+      <script>
+        viewmodel = {
+          rows: new u.DataTable({
+            meta: {
+              id: '',
+              name: '',
+              price: '',
+              num: '',
+              total: ''
+            }
+          }).rows,
+          columnsRender: [{
+            title: 'id',
+            field: 'id'
+          }, {
+            title: 'name',
+            field: 'name',
+            type: 'render',
+            renderFn (row) {
+              return '姓名：<span>' + row.getValue("name") + '</span>, 单价：<span>' + row.getValue("price") + '</span>'
+            }
+          }]
+        }
+      </script>
+    `,
     code: `
       <template>
         <y-grid params="
@@ -97,9 +166,15 @@ function init () {
   ko.applyBindings(viewmodel, document.getElementById('app'))
   setTimeout(() => {
     table.setSimpleData([{
-      id: 1,
-      name: 'name',
+      id: 1001,
+      name: '张飞牛肉',
       price: 23,
+      num: 2,
+      total: 46
+    }, {
+      id: 1002,
+      name: '李白猪肉',
+      price: 31,
       num: 2,
       total: 46
     }])
