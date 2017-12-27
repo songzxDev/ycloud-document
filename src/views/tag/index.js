dded9f17573c3d97cc0eafa1a9d6c10dae2f104b
liuxu7 <liuxu7@yonyou.com>
2017-12-27 13:44:10 +0800

    searchtag

--- src/views/tag/index.js
+++ src/views/tag/index.js
@@ -48,6 +48,54 @@ function init () {
         }
       }
     </script>
+    `,
+    `<template>
+      <y-formitem params="label:'不可选择标签:'">
+        <y-searchtag params="tagList: $root.tagList"></y-searchtag>
+      </y-formitem>
+      <y-formitem params="label:'可选择标签:'">
+        <y-searchtag params="enableActive:true,tagList: $root.tagList"></y-searchtag>
+      </y-formitem>
+    </template>
+    <script>
+      var viewmodel = {
+        tagList: ko.observableArray([{
+          title: '北京',
+          value: 1,
+          handleClick: function (item, isActive) {
+            viewmodel.tagClick(item, isActive)
+          }
+        }, {
+          title: '上海',
+          value: 2,
+          handleClick: function (item, isActive) {
+            viewmodel.tagClick(item, isActive)
+          }
+        }, {
+          title: '广州',
+          value: 3,
+          handleClick: function (item, isActive) {
+            viewmodel.tagClick(item, isActive)
+          }
+        }, {
+          title: '不相信',
+          value: 4,
+          handleClick: function (item, isActive) {
+            viewmodel.tagClick(item, isActive)
+          }
+        }, {
+          title: '眼泪',
+          value: 5,
+          handleClick: function (item, isActive) {
+            viewmodel.tagClick(item, isActive)
+          }
+        }]),
+        tagClick (item, isActive) {
+          console.log(item)
+          console.log(isActive)
+        }
+      }
+    </script>
     `],
     item: ko.observable({
       id: '1',
@@ -58,6 +106,42 @@ function init () {
     },
     handleClose: () => {
       console.log('关闭标签')
+    },
+    // 以下为searchtag的vm
+    tagList: ko.observableArray([{
+      title: '北京',
+      value: 1,
+      handleClick: function (item, isActive) {
+        viewmodel.tagClick(item, isActive)
+      }
+    }, {
+      title: '上海',
+      value: 2,
+      handleClick: function (item, isActive) {
+        viewmodel.tagClick(item, isActive)
+      }
+    }, {
+      title: '广州',
+      value: 3,
+      handleClick: function (item, isActive) {
+        viewmodel.tagClick(item, isActive)
+      }
+    }, {
+      title: '不相信',
+      value: 4,
+      handleClick: function (item, isActive) {
+        viewmodel.tagClick(item, isActive)
+      }
+    }, {
+      title: '眼泪',
+      value: 5,
+      handleClick: function (item, isActive) {
+        viewmodel.tagClick(item, isActive)
+      }
+    }]),
+    tagClick (item, isActive) {
+      console.log(item)
+      console.log(isActive)
     }
   }
 
