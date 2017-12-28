0612704bdf6e8a17f617b1700bd578d1159d1aaf
windknow <songhlc@yonyou.com>
2017-12-28 10:33:10 +0800

    添加grid组件更多demo

--- src/views/grid/index.js
+++ src/views/grid/index.js
@@ -8,7 +8,18 @@ function init () {
       name: '',
       price: '',
       num: '',
-      total: ''
+      total: '',
+      date: ''
+    }
+  })
+  var morerows = new window.u.DataTable({
+    meta: {
+      id: '',
+      name: '',
+      price: '',
+      num: '',
+      total: '',
+      date: ''
     }
   })
   table.on('price.valueChange', function (obj) {
@@ -18,7 +29,44 @@ function init () {
     obj.rowObj.ref('total')(total)
   })
   var viewmodel = {
+    rowspancol: [
+      {
+        title: '<div title="第一列">第一列</div>',
+        width: '100',
+        field: 'field1',
+        type: 'render',
+        renderFn: function (row) {
+          let template = `<div>${row.field1}</div>
+          <span>第一列是由多列组成</span>
+        `
+          return template
+        }
+      }, {
+        title: '第二列',
+        width: '100',
+        field: 'field2'
+      }, {
+        title: '第三列',
+        width: '100',
+        field: 'field3'
+      }, {
+        title: '第四列折行的列名',
+        width: '100',
+        field: 'field4',
+        align: 'right'
+      }, {
+        title: '第五列',
+        width: '50%',
+        field: 'field5'
+      }, {
+        title: '第六列',
+        width: '50%',
+        field: 'field6'
+      }
+    ],
+    rowspanrows: ko.observableArray(),
     rows: table.rows,
+    morerows: morerows.rows,
     columnsRender: [
       {
         title: 'id',
@@ -28,7 +76,7 @@ function init () {
         field: 'name',
         type: 'render',
         renderFn (row) {
-          return `姓名：<span>${row.getValue('name')}</span>, 单价：<span>${row.getValue('price')}</span>`
+          return `姓名：<span>${row.getValue('name')}</span>, 单价：<span>${row.ref('price')()}</span>`
         }
       }
     ],
@@ -59,6 +107,104 @@ function init () {
         field: 'name'
       }
     ],
+    columnsOper: [
+      {
+        title: '序号',
+        field: 'id',
+        type: 'index',
+        width: '80'
+      }, {
+        title: 'name',
+        field: 'name'
+      }, {
+        type: 'operation',
+        operationList: [
+          {
+            title: '切换disabled',
+            click: function (row, evt) {
+              row._disabled(true)
+              return false
+            },
+            visible: function (row) {
+              return !row._disabled()
+            }
+          }, {
+            title: '操作2',
+            click: function (row, evt) {
+              row._disabled(false)
+              return false
+            }
+          }, {
+            title: '操作3',
+            click: function (row, evt) {
+              return false
+            }
+          }, {
+            title: '操作4',
+            click: function (row, evt) {
+              alert('操作4')
+            }
+          }
+        ]
+      }
+    ],
+    selectList: ko.observableArray([
+      {
+        label: '23',
+        value: 23
+      }, {
+        label: '31',
+        value: 31
+      }, {
+        label: '100',
+        value: 100
+      }, {
+        label: '200',
+        value: 200
+      }, {
+        label: '300',
+        value: 300
+      }
+    ]),
+    columnsComp: [{
+      title: 'y-input组件',
+      type: 'component',
+      compFn: function (row) {
+        return {
+          name: 'y-input',
+          params: {
+            value: row.ref('name')
+          }
+        }
+      }
+    }, {
+      title: 'y-datepicker组件',
+      type: 'component',
+      compFn: function (row) {
+        return {
+          name: 'y-datepicker',
+          params: {
+            data: row.ref('date')
+          }
+        }
+      }
+    }, {
+      title: 'y-select组件',
+      type: 'component',
+      compFn: function (row) {
+        return {
+          name: 'y-select',
+          params: {
+            dataList: viewmodel.selectList,
+            id: row.ref('price'),
+            label: row.ref('price')
+          }
+        }
+      }
+    }],
+    handleChange (pageIndex, pageSize) {
+      ycloud.notice.info('pageIndex:' + pageIndex + ' pageSize：' + pageSize)
+    },
     columns: [{
       title: 'id',
       field: 'id'
@@ -114,6 +260,140 @@ function init () {
         }
       </script>
     `,
+    code3: `
+      <template>
+        <y-grid params="isDataTable:true,rows:$parent.rows, columns: $parent.columnsOper"></y-grid>
+      </template>  
+      <script>
+        viewmodel = {
+          rows: new u.DataTable({
+            meta: {
+              id: '',
+              name: '',
+              price: '',
+              num: '',
+              total: ''
+            }
+          }).rows,
+          columnsOper: [{
+              title: '序号',
+              field: 'id',
+              type: 'index',
+              width: '80'
+            }, {
+              title: 'name',
+              field: 'name'
+            }, {
+              type: 'operation',
+              operationList: [
+                {
+                  title: '切换disabled',
+                  click: function (row, evt) {
+                    row._disabled(true)
+                    return false
+                  },
+                  visible: function (row) {
+                    return !row._disabled()
+                  }
+                }, {
+                  title: '操作2',
+                  click: function (row, evt) {
+                    row._disabled(false)
+                    return false
+                  }
+                }, {
+                  title: '操作3',
+                  click: function (row, evt) {
+                    return false
+                  }
+                }, {
+                  title: '操作4',
+                  click: function (row, evt) {
+                    alert('操作4')
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      </script>
+    `,
+    code4: `
+      <template>
+        <y-grid params="onPageChange:handleChange,onSizeChange:handleChange,pagination:true,totalCount:ko.observable(20),maxheight:'auto',isDataTable:true,rows:$parent.rows, columns: $parent.columnsBase"></y-grid>
+      </template>  
+      <script>
+        viewmodel = {
+          rows: new u.DataTable({
+            meta: {
+              id: '',
+              name: '',
+              price: '',
+              num: '',
+              total: ''
+            }
+          }).rows,
+          handleChange (pageIndex, pageSize) {
+            ycloud.notice.info('pageIndex:' + pageIndex + ' pageSize:' + pageSize)
+          },
+        }
+      </script>
+    `,
+    code5: `
+      <template>
+        <y-grid params="maxheight:'200px',lockhead:true, isDataTable:true,rows:$parent.morerows, columns: $parent.columnsBase"></y-grid>
+      </template>
+    `,
+    code6: `
+      <template>
+        <y-grid params="maxheight:'auto',isDataTable:true,rows:$parent.rows, columns: $parent.columnsComp"></y-grid>
+      </template>  
+      <script>
+        viewmodel = {
+          columnsComp: [{
+          title: '姓名',
+          type: 'component',
+          compFn: function (row) {
+            return {
+              name: 'y-input',
+              params: {
+                value: row.ref('name')
+              }
+            }
+          }
+        }, {
+          title: '日期',
+          type: 'component',
+          compFn: function (row) {
+            return {
+              name: 'y-datepicker',
+              params: {
+                data: row.ref('date')
+              }
+            }
+          }
+        }, {
+          title: '单价',
+          type: 'component',
+          compFn: function (row) {
+            return {
+              name: 'y-select',
+              params: {
+                dataList: viewmodel.selectList,
+                id: row.ref('price'),
+                label: row.ref('price')
+              }
+            }
+          }
+        }],
+        }
+      </script>
+    `,
+    code7: `
+      <template>
+        <y-grid params="maxheight:'auto',isStripe:true,rowspan:{maxCol: 2},id:'rowspantable',columns:$root.rowspancol,rows: $root.rowspanrows"></y-grid>
+      </template>
+    `,
     code: `
       <template>
         <y-grid params="
@@ -165,6 +445,117 @@ function init () {
   }
   ko.applyBindings(viewmodel, document.getElementById('app'))
   setTimeout(() => {
+    morerows.setSimpleData([
+      {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }, {
+        id: 1001,
+        name: '张飞牛肉',
+        price: 23,
+        num: 2,
+        total: 46
+      }, {
+        id: 1002,
+        name: '李白猪肉',
+        price: 31,
+        num: 2,
+        total: 46
+      }
+    ])
     table.setSimpleData([{
       id: 1001,
       name: '张飞牛肉',
@@ -178,6 +569,58 @@ function init () {
       num: 2,
       total: 46
     }])
+    viewmodel.rowspanrows([
+      {
+        field1: 'A',
+        field2: 'A',
+        field3: 'C',
+        field4: 'D',
+        field5: 'E',
+        field6: 'F'
+      }, {
+        field1: 'A',
+        field2: 'A',
+        field3: 'C',
+        field4: 'D',
+        field5: 'E',
+        field6: 'F1'
+      }, {
+        field1: 'A',
+        field2: 'C',
+        field3: 'D',
+        field4: 'D',
+        field5: 'E',
+        field6: 'F2'
+      }, {
+        field1: 'A',
+        field2: 'C',
+        field3: 'E',
+        field4: 'D',
+        field5: 'E',
+        field6: 'F'
+      }, {
+        field1: 'B',
+        field2: 'B',
+        field3: 'C',
+        field4: 'D',
+        field5: 'E',
+        field6: 'F'
+      }, {
+        field1: 'B',
+        field2: 'B',
+        field3: 'C',
+        field4: 'D',
+        field5: 'E',
+        field6: 'F'
+      }, {
+        field1: 'B',
+        field2: 'B',
+        field3: 'C',
+        field4: 'D',
+        field5: 'E',
+        field6: 'F'
+      }
+    ])
   }, 300)
 }
 export default {
