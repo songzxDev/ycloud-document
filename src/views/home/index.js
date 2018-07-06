1aba789123290dbc71de1823c206ba187b2b2fb1
windknow <songhlc@yonyou.com>
2018-07-06 13:46:13 +0800

    文档首页更新

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -3,6 +3,29 @@ import ko from 'knockout'
 import '@/components'
 function init () {
   var viewmodel = {
+    curStateIndex: ko.observable(0),
+    handleClick: function (state) {
+
+    },
+    moreText: ko.observable('2.支持对文本超长的特定处理方式，当文字超过指定的长度之后，超出部分会被自动隐藏'),
+    stateTabsItems: ko.observableArray([
+      {
+        title: '容器组件',
+        state: 1
+      },
+      {
+        title: '吸顶菜单',
+        state: 2
+      },
+      {
+        title: '企业级表格组件',
+        state: 3
+      },
+      {
+        title: '深度场景应用',
+        state: 4
+      }
+    ]),
     code: `
       <!-- import stylesheet -->
       <link rel="stylesheet" href="https://yc.yonyoucloud.com/cpu-cdn/js/ycloud/2.0.x/ycloud.min.css">
